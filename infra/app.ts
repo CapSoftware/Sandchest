e1977d144c68e01ca2865fd92f860e970fbad161
Richie McIlroy <33632126+richiemcilroy@users.noreply.github.com>
2026-02-22 23:35:25 +0000

    refactor(infra): simplify app config const assertions

--- infra/app.ts
+++ infra/app.ts
@@ -1,13 +1,13 @@
 export function getAppConfig(stage: string) {
   return {
     name: "sandchest",
-    home: "aws" as const,
+    home: "aws",
     providers: {
       aws: {
         region: "us-east-1",
       },
     },
-    removal: stage === "production" ? ("retain" as const) : ("remove" as const),
+    removal: stage === "production" ? "retain" : "remove",
     protect: stage === "production",
-  };
+  } as const;
 }
